8b568744b798bd86c122c58e91c38bada46fcaf6
ANDRES FELIPE ESTAN MARQUEZ <aestanm@gmail.com>
2023-09-10 21:56:07 -0500

    consulta de vehiculo por placa en api

--- lib/pages/findPlate.dart
+++ lib/pages/findPlate.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'newInspection.dart';
 import '../widgets/appTheme.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class FindPlatePage extends StatefulWidget {
   @override
@@ -12,14 +14,86 @@ class _FindPlatePageState extends State<FindPlatePage> {
   TextEditingController _plateController = TextEditingController();
   bool _isPlateEmpty = true;
 
-void _navigateToNewInspection() {
-  Navigator.pushReplacement(
-    context,
-    MaterialPageRoute(
-      builder: (context) => NewInspection(plateValue: _plateController.text),
-    ),
-  );
-}
+  void _navigateToNewInspection() async {
+    final plate = _plateController.text;
+
+    try {
+      // Realizar la solicitud POST a la API
+      final response = await http.post(
+        Uri.parse('https://ibingcode.com/public/infovehiculo'),
+        body: '{"placa": "$plate"}',
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 200) {
+        if (response.body.isEmpty) {
+          // Mostrar el mensaje cuando no se encuentren datos
+          _showErrorMessage(
+              'Vehículo no encontrado en la base de datos del RUNT.');
+        } else {
+          final List<dynamic> data = json.decode(response.body);
+
+          if (data.isNotEmpty) {
+            final Map<String, dynamic> vehicleData = data[0];
+
+            String modelo = vehicleData['v.modelo'];
+            String numero_chasis = vehicleData['numero_chasis'];
+            String numero_motor = vehicleData['numero_motor'];
+            String marca = vehicleData['marca'];
+            String tipo_servicio = vehicleData['tipo_servicio'];
+            String tipo_vehiculo = vehicleData['tipo_vehiculo'];
+            String organismo_transito = vehicleData['organismo_transito'];
+            String id_propietario = vehicleData['id_propietario'].toString();
+            String nombre_propietario = vehicleData['nombre_propietario'];
+
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NewInspection(
+                    plateValue: plate,
+                    modelo: modelo,
+                    numero_chasis: numero_chasis,
+                    numero_motor: numero_motor,
+                    marca: marca,
+                    tipo_servicio: tipo_servicio,
+                    tipo_vehiculo: tipo_vehiculo,
+                    organismo_transito: organismo_transito,
+                    id_propietario: id_propietario,
+                    nombre_propietario: nombre_propietario),
+              ),
+            );
+          } else {
+            _showErrorMessage(
+                'Vehículo no encontrado en la base de datos del RUNT.');
+          }
+        }
+      } else {
+        _showErrorMessage(
+            'No se pudo obtener información del vehículo. Sin conexión con el servidor.');
+      }
+    } catch (e) {
+      _showErrorMessage(
+          'Error al comunicarse con el servidor. Verifique su conexión a internet.'+e.toString());
+    }
+  }
+
+  void _showErrorMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error', style: TextStyle(fontSize: 25.0)),
+        content: Text(message, style: TextStyle(fontSize: 25.0)),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Aceptar', style: TextStyle(fontSize: 25.0)),
+            onPressed: () {
+              Navigator.of(context).pop(); // Cierra la ventana emergente
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   void initState() {
@@ -62,14 +136,14 @@ void _navigateToNewInspection() {
                     controller: _plateController,
                     textAlign: TextAlign.center,
                     inputFormatters: [
-                      LengthLimitingTextInputFormatter(6), // Limita a 6 caracteres
-                      UpperCaseTextFormatter(), // Convierte a mayúsculas
+                      LengthLimitingTextInputFormatter(6),
+                      UpperCaseTextFormatter(),
                     ],
                     focusNode: primaryFocus,
                     decoration: InputDecoration(
-                      hintText: 'INGRESE LA PLACA DEL VEHICULO',
+                      hintText: 'INGRESE LA PLACA DEL VEHÍCULO',
                     ),
-                    style: AppTheme().bodyLargeStyle,
+                    style: TextStyle(fontSize: 25.0),
                   ),
                 ),
               ),
@@ -77,7 +151,7 @@ void _navigateToNewInspection() {
             ElevatedButton(
               style: AppTheme().buttonLightStyle,
               onPressed: _isPlateEmpty ? null : _navigateToNewInspection,
-              child: Text('Inspeccionar', style: AppTheme().textButton1),
+              child: Text('Inspeccionar', style: TextStyle(fontSize: 25.0)),
             ),
           ],
         ),
@@ -93,4 +167,10 @@ class UpperCaseTextFormatter extends TextInputFormatter {
     return TextEditingValue(
         text: newValue.text.toUpperCase(), selection: newValue.selection);
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: FindPlatePage(),
+  ));
 }
--- lib/pages/newInspection.dart
+++ lib/pages/newInspection.dart
@@ -3,17 +3,39 @@ import 'package:geolocator/geolocator.dart';
 
 class NewInspection extends StatelessWidget {
   final String plateValue;
+  final String modelo;
+  final String numero_chasis;
+  final String numero_motor;
+  final String marca;
+  final String tipo_servicio;
+  final String tipo_vehiculo;
+  final String organismo_transito;
+  final String id_propietario;
+  final String nombre_propietario;
 
-  NewInspection({required this.plateValue});
+  NewInspection({
+    required this.plateValue,
+    required this.modelo,
+    required this.numero_chasis,
+    required this.numero_motor,
+    required this.marca,
+    required this.tipo_servicio,
+    required this.tipo_vehiculo,
+    required this.organismo_transito,
+    required this.id_propietario,
+    required this.nombre_propietario,
+  });
 
   @override
   Widget build(BuildContext context) {
     String firstPart = plateValue.substring(0, 3);
     String secondPart = plateValue.substring(3, 6);
 
+    TextStyle textStyle = TextStyle(fontSize: 25.0);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('New Inspection'),
+        title: Text('New Inspection', style: textStyle),
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -23,96 +45,118 @@ class NewInspection extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            decoration: BoxDecoration(
-              color: Colors.grey.withOpacity(0.4),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    // Contenedor para datos fijos
-                    Container(
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Imagen de la placa y texto
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/placa.png',
+                        width: 350,
+                        height: 200,
                       ),
-                      child: Column(
-                        children: [
-                          Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              Image.asset(
-                                'assets/placa.png',
-                                width: 350,
-                                height: 350,
-                              ),
-                              Positioned(
-                                left: 25,
-                                child: Container(
-                                  width: 150,
-                                  child: Text(
-                                    firstPart,
-                                    style: TextStyle(
-                                        fontSize: 68, fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                right: 2,
-                                child: Container(
-                                  width: 150,
-                                  child: Text(
-                                    secondPart,
-                                    style: TextStyle(
-                                        fontSize: 68, fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          
-                          Text('Tipo de servicio: Particular'),
-                          Text('Marca: Chevrolet'),
-                          Text('Modelo: Swift'),
-                          Text('Ubicación física: Coordenadas de ejemplo'),
-                        ],
-                      ),
-                    ),
-
-                    SizedBox(height: 16),
-
-                    // Contenedor para campos editables
-                    Container(
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        children: [
-                          TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Tipo vehículo abandonado',
+                      Positioned(
+                        left: 25,
+                        child: Container(
+                          width: 150,
+                          child: Text(
+                            firstPart,
+                            style: TextStyle(
+                              fontSize: 68,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          SizedBox(height: 8),
-                          // Otros campos editables aquí
-                        ],
+                        ),
                       ),
+                      Positioned(
+                        right: 2,
+                        child: Container(
+                          width: 150,
+                          child: Text(
+                            secondPart,
+                            style: TextStyle(
+                              fontSize: 68,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  // Contenedor para datos fijos
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  ],
-                ),
+                    child: Column(
+                      children: [
+                        _buildDataRow('Tipo de servicio', tipo_servicio),
+                        _buildDataRow('Marca', marca),
+                        _buildDataRow('Modelo', modelo),
+                        _buildDataRow('Número de Chasis', numero_chasis),
+                        _buildDataRow('Número de Motor', numero_motor),
+                        _buildDataRow('Tipo de Vehículo', tipo_vehiculo),
+                        _buildDataRow('Organismo de Tránsito', organismo_transito),
+                        _buildDataRow('ID de Propietario', id_propietario),
+                        _buildDataRow('Nombre de Propietario', nombre_propietario),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  // Contenedor para campos editables
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Tipo vehículo abandonado',
+                          ),
+                          style: textStyle,
+                        ),
+                        SizedBox(height: 8),
+                        // Otros campos editables aquí
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildDataRow(String label, String value) {
+    return Row(
+      children: [
+        Text(
+          '$label',
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          ': $value',
+          style: TextStyle(
+            fontSize: 25.0,
+          ),
+        ),
+      ],
     );
   }
 }
